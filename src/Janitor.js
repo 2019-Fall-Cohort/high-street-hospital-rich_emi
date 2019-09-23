b2e8ed132348f20d879b05f059716147eed58fd3
richard wilson <rcwilson88@gmail.com>
2019-09-23 00:44:41 -0400

    index.js started. Hospital has a starting crew method

--- src/Janitor.js
+++ src/Janitor.js
@@ -2,7 +2,7 @@ const Employee = require("./Employee");
 
 class Janitor extends Employee {
 
-        constructor(position = `Janitor`, name, id, salary = 40000){
+        constructor(position = `Janitor`, name, id, salary = `$40,000`){
             super(position, name, id, salary)
 
             this.isSweeping = false;
